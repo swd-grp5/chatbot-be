84cccc0c79d3ff0ae55b6b00c25cbec46a330048
karmalinh <142292959+karmalinh@users.noreply.github.com>
2026-05-30 16:04:39 +0700

    fix resoponse

--- be/src/main/java/swdchatbox/system/auth/dto/request/RegisterRequest.java
+++ be/src/main/java/swdchatbox/system/auth/dto/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package swdchatbox.system.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,5 +18,9 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank(message = "Please confirm your password")
+    private String confirmPassword;
 }
