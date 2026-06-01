b3450f131002bb138be1272884923e7d79669bdb
Phạm Công Định <dinhp5432@gmail.com>
2026-06-02 00:50:27 +0700

    add google client id

--- be/src/main/java/swdchatbox/config/SecurityConfig.java
+++ be/src/main/java/swdchatbox/config/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:8081"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
