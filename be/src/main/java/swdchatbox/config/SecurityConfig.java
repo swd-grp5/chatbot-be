60f81a40fb0349e082974fab3687b6cbca5e7486
karmalinh <142292959+karmalinh@users.noreply.github.com>
2026-06-02 03:56:18 +0700

    test 2

--- be/src/main/java/swdchatbox/config/SecurityConfig.java
+++ be/src/main/java/swdchatbox/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/auth/register",
                                 "/api/auth/login",
                                 "/api/auth/google",
